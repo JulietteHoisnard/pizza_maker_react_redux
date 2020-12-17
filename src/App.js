84d2074089617d265010e4940bd751ea8b8f7751
Juliette Hoisnard <juliette@hoisnard.fr>
2020-12-18 00:51:16 +0100

    stepper function and display

--- src/App.js
+++ src/App.js
@@ -3,31 +3,93 @@ import { PizzaForm } from "./features/pizza/PizzaForm";
 
 import "./App.css";
 import { Container } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import {
+  selectDough,
+  selectStyle,
+  selectToppings,
+} from "./features/pizza/pizzaSlice";
 
 function App() {
+  const style = useSelector(selectStyle);
+  const dough = useSelector(selectDough);
+  const toppings = useSelector(selectToppings);
+
   return (
     <div className="App">
-      <Container className="App-header">
-        <PizzaForm options={["Italian", "American"]} type="style" />
-        <PizzaForm
-          options={["Standard", "Wholegrain", "Sour dough"]}
-          type="dough"
-        />
-        <PizzaForm
-          options={[
-            "Cheese",
-            "Ham",
-            "Mozzarella",
-            "Pineapple",
-            "Mushrooms",
-            "Mais",
-            "Onions",
-            "Garlic",
-            "Pickles",
-            "More cheese",
-          ]}
-          type="toppings"
-        />
+      <Container className="py-5 p-2">
+        <h1 className="my-5">Welcome to pizzapizz'</h1>
+        <div className="my-5 d-flex  justify-content-around">
+          <h3
+            className={
+              !style
+                ? "rounded-sm p-3 mb-2 bg-dark text-white"
+                : "p-3 mb-2 text-muted"
+            }
+          >
+            Step1
+          </h3>
+          <h3
+            className={
+              style && !dough
+                ? "rounded-sm p-3 mb-2 bg-dark text-white"
+                : "p-3 mb-2 text-muted"
+            }
+          >
+            Step2
+          </h3>
+          <h3
+            className={
+              style && dough && toppings.length === 0
+                ? "rounded-sm p-3 mb-2 bg-dark text-white"
+                : "p-3 mb-2 text-muted"
+            }
+          >
+            Step3
+          </h3>
+        </div>
+        {!style && <PizzaForm options={["Italian", "American"]} type="style" />}
+        {style && !dough && (
+          <PizzaForm
+            options={["Standard", "Wholegrain", "Sour dough"]}
+            type="dough"
+          />
+        )}
+        {style && dough && toppings.length === 0 && (
+          <PizzaForm
+            options={[
+              "Cheese",
+              "Ham",
+              "Mozzarella",
+              "Pineapple",
+              "Mushrooms",
+              "Mais",
+              "Onions",
+              "Garlic",
+              "Pickles",
+              "More cheese",
+            ]}
+            type="toppings"
+          />
+        )}
+        {style && dough !== "" && toppings.length > 0 && (
+          <h2>Congrats! Your order is confirmed.</h2>
+        )}
+        <h2>Your selection</h2>
+        {style && <h3>Style: {style}</h3>}
+        {style && dough && <h3>Dough type: {dough}</h3>}
+        <>
+          {style && dough !== "" && toppings.length > 0 && (
+            <>
+              <h3>Toppings: </h3>
+              <ul>
+                {toppings.map((topping) => (
+                  <li>{topping}</li>
+                ))}
+              </ul>
+            </>
+          )}
+        </>
       </Container>
     </div>
   );
--- src/features/pizza/PizzaForm.js
+++ src/features/pizza/PizzaForm.js
@@ -2,58 +2,75 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { choiceDough, choiceStyle, choiceToppings } from "./pizzaSlice";
 import { Button, Container, Form } from "react-bootstrap";
+import store from "../../app/store";
 
 export const PizzaForm = (options) => {
   const dispatch = useDispatch();
   console.log(options.options);
+  console.log(options.options[0]);
   console.log(options.type);
-  console.log(options.action);
 
   const [selectedChoice, setSelectedChoice] = useState(options.options[0]);
+  const selectedChoices = [];
+
+  const onTickedFoodChange = (e) => {
+    const checked = e.target.checked;
+    const value = e.target.name;
+    console.log(checked);
+    if (checked) selectedChoices.push(value);
+  };
+  const checkedOption = options.options[0] === "cheese";
 
   const handleSubmit = (event) => {
     event.preventDefault();
     if (options.type === "style") dispatch(choiceStyle(selectedChoice));
     if (options.type === "dough") dispatch(choiceDough(selectedChoice));
-    if (options.type === "toppings") dispatch(choiceToppings([selectedChoice]));
+    if (options.type === "toppings") {
+      dispatch(choiceToppings(selectedChoices));
+      console.log(store.getState());
+    }
   };
   return (
     <Container className="container-md mx-auto">
       <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="Form.ControlParticipantIdentity">
-          <Form.Label>Pizza {options.type}:</Form.Label>
-          {options.type !== "toppings" && (
-            <Form.Control
-              as="select"
-              value={selectedChoice}
-              onChange={(e) => setSelectedChoice(e.target.value)}
-            >
-              {options.options.map((option) => (
-                <option key={option} value={option}>
-                  {option}
-                </option>
-              ))}
-            </Form.Control>
-          )}
-          {options.type === "toppings" && (
-            <>
-              {options.options.map((option) => (
-                <>
-                  <Form.Label>{option}:</Form.Label>
-
-                  <Form.Control
-                    type="checkbox"
-                    name="city"
-                    value={option}
-                    onChange={(e) => setSelectedChoice(e.target.value)}
-                  />
-                </>
-              ))}
-            </>
-          )}
-        </Form.Group>
-        <div style={{ textAlign: "right" }}>
-          <Button type="submit">Validate!</Button>
+        <div className="d-flex">
+          <Form.Group controlId="Form.ControlParticipantIdentity">
+            <h2>Pizza {options.type}:</h2>
+            {options.type !== "toppings" && (
+              <Form.Control
+                as="select"
+                value={selectedChoice}
+                onChange={(e) => setSelectedChoice(e.target.value)}
+              >
+                {options.options.map((option) => (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </Form.Control>
+            )}
+            {options.type === "toppings" && (
+              <>
+                {options.options.map((option) => (
+                  <>
+                    <Form.Label>{option}:</Form.Label>
+                    <Form.Control
+                      type="checkbox"
+                      defaultChecked={checkedOption}
+                      name={option}
+                      key={option}
+                      onChange={onTickedFoodChange}
+                    />{" "}
+                  </>
+                ))}
+              </>
+            )}
+          </Form.Group>
+          <div style={{ textAlign: "right" }}>
+            <Button type="submit">
+              {options.type === "toppings" ? "Order now!" : "Validate!"}
+            </Button>
+          </div>
         </div>
       </Form>
     </Container>
